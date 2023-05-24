16c9d11777e15b3b55cc38c9a8658931b6c19590
alopez-2018459 <alopez-2018459@kinal.edu.gt>
2023-05-23 23:32:44 -0600

    added toplevel await and mongoose configs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+
+    return config;
+  },
   images: {
     domains: ["images.unsplash.com"],
   },
+  experimental: {
+    serverComponentsExternalPackages: ["mongoose"],
+  }
 };
 
 module.exports = nextConfig;
