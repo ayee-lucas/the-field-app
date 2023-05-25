8ac4d9fc6f9e4ebaf2211f43e30833d3c20dce8a
alopez-2018459 <alopez-2018459@kinal.edu.gt>
2023-05-25 00:32:34 -0600

    Image Optimization and fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,32 @@
 import Link from "next/link";
 import Navbar from "./components/navbar";
 import About from "./components/HeroMain/About";
+import Image from "next/image";
+import bg from "../public/images/Background/main-bg.jpeg"
 import { poppins } from "./fonts";
 import { IoIosArrowDown } from "react-icons/io";
 
 export default function Home() {
   return (
     <>
-      <main className="flex relative min-h-screen flex-col items-center main-bg-img bg-fixed bg-cover bg-no-repeat">
+      <main className="flex relative min-h-screen flex-col items-center bg-black">
+        {/** Background */}
+        <Image 
+        src={bg}
+        alt="background"
+        fill
+        placeholder="blur"
+        className="absolute w-full h-full object-cover"
+        priority
+
+        />
+        
         {/** Overlay */}
-        <div className="absolute inset-0 bg-black/70 z-[1]"></div>
+        <div className="absolute inset-0 bg-black/90 z-[1]"></div>
         <Navbar />
         <div className="flex flex-col items-start px-5 justify-center z-20 h-[900px] w-full text-white">
           <h1 className={`${poppins.className} text-6xl font-medium`}>
-            Discovering Sports Talent
+            <span className="text-fieldGreen">Discovering</span> Sports Talent
           </h1>
           <h3 className="py-3 text-xl">
             The Global Platform for Talent and Sports Organizations
