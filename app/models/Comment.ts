2d68b43968def53f6a59e2076972938710dc4a74
falegria-2018246 <falegria-2018246@kinal.edu.gt>
2023-05-25 17:22:06 -0600

    Modelo de comment

---
+++ app/models/Comment.ts
@@ -0,0 +1,39 @@
+import { Document, Schema, model, models } from "mongoose";
+import { IUser } from "./User";
+import { IPost } from "./Post";
+
+// Interface for Comment document
+export interface IComment extends Document {
+  author: Schema.Types.ObjectId;
+  post: Schema.Types.ObjectId;
+  content: string;
+  createdAt: Date;
+}
+
+// Mongoose schema for Comment
+const commentSchema = new Schema<IComment>(
+  {
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Author is required."],
+    },
+    post: {
+      type: Schema.Types.ObjectId,
+      ref: "Post",
+      required: [true, "Post is required."],
+    },
+    content: {
+      type: String,
+      required: [true, "Content is required."],
+    },
+  },
+  {
+    timestamps: true, versionKey: false
+  }
+);
+
+// Create and export the Comment model
+const Comment = models.Comment || model<IComment>("Comment", commentSchema);
+
+export default Comment;
