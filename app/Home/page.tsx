76b4a7f55e48ae2b5041902362b70d5269af6e49
jmorales-2018317 <jmorales-2018317@kinal.edu.gt>
2023-05-26 11:59:14 -0600

    Home design

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -1,5 +1,5 @@
 export default function Page(){
     return (
-        <div>Home</div>
+        <div className="fixed top-[9vh] left-[240px] max-md:left-0 w-full z-50 h-screen p-4">Home</div>
     );
-}
+}   
