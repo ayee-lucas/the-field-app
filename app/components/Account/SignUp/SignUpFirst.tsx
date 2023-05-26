3d7cf29cfa30e613e356881480588b2a87d4585f
alopez-2018459 <alopez-2018459@kinal.edu.gt>
2023-05-26 00:16:11 -0600

    added signup feature

--- app/components/Account/SignUp/SignUpFirst.tsx
+++ app/components/Account/SignUp/SignUpFirst.tsx
@@ -1,26 +1,56 @@
 "use client";
 
-import  { useEffect, useState } from "react";
+import React, { useEffect, useState, useTransition } from "react";
 import FormInput from "./FormInput";
 import { montserrat, roboto } from "@/app/fonts";
 import { AiTwotoneLock } from "react-icons/ai";
+import { signUp } from "@/app/account/signup/actions";
+import { useRouter } from "next/navigation";
 
 const SignUpFirst = () => {
   const [password, setPassword] = useState("");
+
+  const [data, setData] = useState({});
 
   const [confirmPassword, setConfirmPassword] = useState("");
 
   const [validPassword, setValidPassword] = useState(true);
 
+  const [responseError, setResponseError] = useState("");
 
-  const defaultError = true
+  const [defaultError, setDefaultError] = useState(true);
+
+  const router = useRouter();
+
+
+
+  const onClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await signUp(data);
+      console.log(response);
+
+      if (response.message == 'success') {
+        setResponseError("");
+        router.push('/account/signin/')
+      } else {
+        setDefaultError(false);
+        setResponseError(`${response.message}`);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   useEffect(() => {
     if (password !== confirmPassword) {
       /** Password don't match */
       setValidPassword(false);
+      setResponseError("");
     } else {
       setValidPassword(true);
+      setData({ ...data, password: password });
     }
   }, [password, confirmPassword]);
 
@@ -30,16 +60,28 @@ const SignUpFirst = () => {
       className={`${montserrat.className} ${roboto.variable} w-full h-full min-h-[200px] p-2`}
     >
       <h1 className="text-xl py-6 font-medium">ACCOUNT INFORMATION</h1>
+
+      {responseError !== "" ? (
+        <h1 className="text-red-600 text-sm px-2 font-medium italic">
+          {responseError}
+        </h1>
+      ) : null}
       <FormInput
         label="Username"
         placeholder="Enter your username"
         error={defaultError}
         type="text"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setData({ ...data, username: e.target.value })
+        }
       />
       <FormInput
         error={defaultError}
         label="Email"
         placeholder="Enter your email"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setData({ ...data, email: e.target.value })
+        }
         type="email"
       />
       <div className="relative flex justify-between items-center gap-4">
@@ -88,7 +130,8 @@ const SignUpFirst = () => {
         <button
           type="submit"
           disabled={!validPassword ? true : false}
-          className="hover:text-white ml-3  hover:bg-green-600 rounded-lg py-2 px-6 border-green-600 border-[2px] disabled:border-[1px] disabled:hover:bg-transparent transition-all disabled:text-gray-500 disabled:border-black"
+          className="hover:text-white ml-3 w-full hover:bg-green-600 rounded-lg py-2 px-6 border-green-600 border-[2px] disabled:border-[1px] disabled:hover:bg-transparent transition-all disabled:text-gray-500 disabled:border-black"
+          onClick={(e) => onClick(e)}
         >
           Sign Up
         </button>
