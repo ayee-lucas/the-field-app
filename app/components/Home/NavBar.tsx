25bf2f9fe45c1dfcf89d271b7a8088f906bf7a2a
llopez-2021134 <llopez-2021134@kinal.edu.gt>
2023-05-26 08:54:15 -0600

    navBar changes

--- app/components/Home/NavBar.tsx
+++ app/components/Home/NavBar.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { AiOutlineMenu } from "react-icons/ai"
 
 export const NavBar = () => {
   return (
     <nav className="bg-white border-gray-200 dark:bg-black">
       <div className="min-h-[15vh] flex flex-wrap items-center justify-between mx-auto p-4 px-6">
         <Link href="/Home" className="flex items-center">
-          <div className="flex img-toggle mr-3" />
-          <span className="self-center text-4xl font-semibold whitespace-nowrap dark:text-white max-sm:block max-md:text-lg">
+          <div className="flex img-toggle mr-3 max-lg:mr-0 max-sm:hidden" />
+          <AiOutlineMenu/>
+          <span className="self-center text-4xl font-semibold whitespace-nowrap dark:text-white max-sm:block max-md:text-3xl">
             THE <span className="text-fieldGreen">FIELD</span>
           </span>
         </Link>
@@ -40,13 +42,13 @@ export const NavBar = () => {
               <input
                 type="text"
                 id="simple-search"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-l-lg focus:ring-blue-500 focus:border-blue-500 block w-96 max-md:w-auto pl-10 p-2.5  dark:bg-black dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-l-lg block w-96 max-md:w-auto pl-10 p-2.5  dark:bg-black dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Search"
               />
             </div>
             <button
               type="submit"
-              className="p-2.5 text-sm font-medium text-white bg-fieldGreen rounded-r-lg border border-[#37a33f] dark:boder-gray-500 hover:bg-[#37a33f] focus:outline-none dark:bg-black dark:hover:bg-[#37a33f]"
+              className="p-2.5 -ml-1 text-sm font-medium text-white bg-fieldGreen rounded-r-lg border border-[#37a33f] dark:border-gray-500 hover:bg-[#37a33f] dark:hover:border-[#37a33f] focus:outline-none dark:bg-black dark:hover:bg-[#37a33f]"
             >
               <svg
                 className="w-5 h-5"
@@ -65,7 +67,28 @@ export const NavBar = () => {
             </button>
           </form>
         </div>
-        <div className="flex items-center">
+        <div>
+        <button
+              type="submit"
+              className="sm:hidden p-2.5  text-white bg-fieldGreen rounded-r-lg hover:bg-[#37a33f] dark:hover:border-[#37a33f] focus:outline-none dark:bg-black dark:hover:bg-[#37a33f]"
+            >
+              <svg
+                className="w-7 h-7"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                ></path>
+              </svg>
+            </button>
+        </div>
+        <div className="flex items-center max-sm:hidden  ">
           <button
             type="button"
             className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
@@ -75,7 +98,7 @@ export const NavBar = () => {
             data-dropdown-placement="bottom"
           >
             <span className="sr-only">Open user menu</span>
-            <Image className="w-8 h-8 rounded-full" src="" alt="user photo" />
+            <Image className="w-10 h-10 rounded-full" src="" width={500} height={500} alt="user photo" />
           </button>
           <div
             className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
