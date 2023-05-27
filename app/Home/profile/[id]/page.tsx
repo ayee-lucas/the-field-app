477ab0b5792cb66d2aa3c60254c688fa74f9a607
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-05-27 12:57:09 -0600

    fixes

--- app/Home/profile/[id]/page.tsx
+++ app/Home/profile/[id]/page.tsx
@@ -10,7 +10,7 @@ export default async function Page() {
     <div className="w-full h-full flex flex-col p-9">
       <div className="flex flex-col justify-center items-center">
         <div className="relative rounded-md h-full w-[200px] min-h-[200px]">
-          <Image
+        <Image
             src={`${session?.user?.image}`}
             alt="user image"
             fill
