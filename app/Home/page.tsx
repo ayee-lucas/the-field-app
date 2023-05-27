6ce2070d2f9d1432f3e269e0449227e6ae80db5a
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-05-26 18:50:10 -0600

    added link to account profile

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -3,13 +3,14 @@ import { authOptions } from "../api/auth/[...nextauth]/route";
 import Image from "next/image";
 import Link from "next/link";
 
-export default async function Page(){
+export default async function Page() {
+  const session = await getServerSession(authOptions);
 
-    const session = await getServerSession(authOptions);
+  console.log({ session });
 
-    console.log({session})
-
-    return (
-        <div className="fixed top-[9vh] left-[240px] max-md:left-0 w-full z-50 h-screen p-4">Home</div>
-    );
-}   
+  return (
+    <div className="fixed top-[9vh] left-[240px] max-md:left-0 w-full z-50 h-screen p-4">
+      <Link href={`/Home/profile/${session?.user?.id}`}>account</Link>
+    </div>
+  );
+}
