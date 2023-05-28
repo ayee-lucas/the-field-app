319bba2b2880beecc78496e02bfb87617fd80859
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-05-27 21:53:23 -0600

    Added redirect if already logged in

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,32 @@ import Link from "next/link";
 import Navbar from "./components/navbar";
 import About from "./components/HeroMain/About";
 import Image from "next/image";
-import bg from "../public/images/Background/main-bg.jpeg"
+import bg from "../public/images/Background/main-bg.jpeg";
 import { poppins } from "./fonts";
 import { IoIosArrowDown } from "react-icons/io";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await getServerSession(authOptions);
+
+  if (session?.user) redirect("/Home");
+
   return (
     <>
       <main className="flex relative min-h-screen flex-col items-center bg-black">
         {/** Background */}
-        <Image 
-        src={bg}
-        alt="background"
-        fill
-        placeholder="blur"
-        className="absolute w-full h-full object-cover"
-        priority
-
+        <Image
+          src={bg}
+          alt="background"
+          fill
+          placeholder="blur"
+          className="absolute w-full h-full object-cover"
+          priority
         />
-        
+
         {/** Overlay */}
         <div className="absolute inset-0 bg-black/80 z-[1]"></div>
         <Navbar />
@@ -38,8 +45,10 @@ export default function Home() {
             Create an Account
           </Link>
         </div>
-        <IoIosArrowDown size={40} className="absolute bottom-0 inset-x-[50%] z-20 animate-bounce text-white text-6xl" />
-
+        <IoIosArrowDown
+          size={40}
+          className="absolute bottom-0 inset-x-[50%] z-20 animate-bounce text-white text-6xl"
+        />
       </main>
 
       <About />
