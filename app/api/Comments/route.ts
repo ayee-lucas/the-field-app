406a4a1e2e4ad26aaf652be8b404eb49e8470351
falegria-2018246 <falegria-2018246@kinal.edu.gt>
2023-06-01 17:41:48 -0600

    add comments to post array

--- app/api/Comments/route.ts
+++ app/api/Comments/route.ts
@@ -1,7 +1,10 @@
 import { NextResponse, NextRequest } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import dbConnect from "@/app/db/Connection";
 import Comment from "@/app/models/Comment";
 import User from "@/app/models/User";
+import Post from "@/app/models/Post";
 
 dbConnect();
 
@@ -36,42 +39,57 @@ export async function GET(request: NextRequest) {
   }
 
   export async function POST(request: NextRequest) {
+    const session = await getServerSession(authOptions);
     try {
       // Parse the request body as JSON
-      const json = await request.json();
-      console.log({ DataRequest: json });
-  
-      // Validate the required fields in the request body
-      if (!json.author || !json.post || !json.content) {
-        return new NextResponse(
-          JSON.stringify({
-            message: "Missing required fields in the request body.",
-          }),
-          { status: 400 }
-        );
-      }
-  
-      // Create a new notification object with the parsed data
-      const data = new Comment(json);
-      console.log({ NotificationCreated: data });
-  
-      // Save the notification object to the database
-      const comment = await data.save();
-  
-      // Return a NextResponse object with the saved notification data and a 200 status code
-      return new NextResponse(JSON.stringify(comment), {
-        status: 200,
-        headers: { "Content-Type": "application/json" },
-      });
+    const { post, content } = await request.json();
+
+    // Validate the required fields in the request body
+    if (!post || !content) {
+      return new NextResponse(
+        JSON.stringify({
+          message: "Missing required fields in the request body.",
+        }),
+        { status: 400 }
+      );
+    }
+
+    // Get the user session
+    const userId = session?.user?.id;
+
+    // Validate the user session
+    if (!userId) {
+      return new NextResponse(
+        JSON.stringify({
+          message: "User session not found.",
+        }),
+        { status: 401 }
+      );
+    }
+
+   
+    const comment = new Comment({
+      post,
+      content,
+      author: userId,
+    });
+    console.log({ CommentCreated: comment });
+
+    await comment.save();
+
+    const postDocument = await Post.findById(post);
+
+    // Add the comment to the comments array of the post
+    postDocument.comments.push(comment._id);
+    await postDocument.save();
+
+    return new NextResponse(JSON.stringify(comment), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+
     } catch (err) {
-      console.log({ err });
-  
-      // If there is any other error, return a NextResponse object with an error message and a 500 status code
-      const error = {
-        message: "Error saving notification.",
-        error: err,
-      };
-      return new NextResponse(JSON.stringify(error), { status: 500 });
+      return new NextResponse(JSON.stringify(err), { status: 500 });
     }
   }
   
