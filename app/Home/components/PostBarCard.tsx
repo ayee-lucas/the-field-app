e5a5a2a67f67d67148efb5dcf272fd0f973e3b4c
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-06-05 22:50:12 -0600

    Fixes on bar card

--- app/Home/components/PostBarCard.tsx
+++ app/Home/components/PostBarCard.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 import example from '../../../public/images/Background/card_example.jpg';
 
 const PostBarCard = () => (
-  <div className="flex flex-col text-white items-start relative justify-end gap-2 w-full min-h-[250px] max-h-[250px] border border-gray-700 rounded-xl p-4 shadow-lg min-w-[200px] max-w-[200px]">
+  <div className="flex flex-col text-white items-start relative justify-end gap-2 w-full min-h-[250px] max-h-[250px] border border-gray-700 dark:border-zinc-800 rounded-xl p-4 shadow-lg min-w-[200px] max-w-[200px]">
 
     <h1 className="text-lg font-medium">Title</h1>
 
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
 
-    <div className="absolute inset-0 bg-black/40 z-[-1]" />
+    <div className="absolute inset-0 bg-black/40 z-[-1] rounded-lg" />
 
     <Image
       src={example}
