0e1bd7004d7fa85f58052c59decb2eea38961ade
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-06-05 22:49:57 -0600

    Fixes on top feedbar

--- app/Home/components/FeedBar.tsx
+++ app/Home/components/FeedBar.tsx
@@ -1,11 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import PostBarCard from './PostBarCard';
 
 const FeedBar = () => (
-  <div className="w-full flex flex-col gap-2 items-start justify-center">
+  <div className="w-full flex flex-col gap-2 items-start justify-center py-5">
     <h1 className="text-xl font-bold ">Recent</h1>
-    <div className="w-full flex items-center gap-2 relative overflow-x-scroll">
-      {Array.from({ length: 8 }).map((_, index) => (
+    <div className="w-full flex items-center justify-between gap-2 overflow-x-auto">
+      {Array.from({ length: 5 }).map((_, index) => (
         <PostBarCard />
       ))}
 
