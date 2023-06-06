52966bda9b5414801a0147fe045dc75dfed8f3e0
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-06-05 22:49:28 -0600

    Added static posts cards

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -1,18 +1,18 @@
-import { getServerSession } from 'next-auth';
-import Link from 'next/link';
-import { authOptions } from '../api/auth/[...nextauth]/route';
 import FeedBar from './components/FeedBar';
+import ClientNewPost from './components/ClientNewPost';
+import PostHomeCard from './components/PostHomeCard';
 
 export default async function Page() {
-  const session = await getServerSession(authOptions);
-
   return (
-    <div className="w-full h-screen dark:bg-black p-2 dark:text-white">
-      <h1 className="text-4xl py-3 font-bold">
+    <div className="w-full min-h-screen h-full dark:bg-black  dark:text-white py-10 px-[3rem]">
+      <h1 className="text-4xl py-3 font-bold dark:text-white">
         Feed
       </h1>
-
       <FeedBar />
+      <ClientNewPost />
+      {Array.from({ length: 10 }, (_, i) => (
+        <PostHomeCard key={i} />
+      ))}
     </div>
   );
 }
