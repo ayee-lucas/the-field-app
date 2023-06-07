445c65bba7a9ce0e05776aac852913695d987eca
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-06-06 23:59:30 -0600

    fixes

--- app/Home/components/PostBarCard.tsx
+++ app/Home/components/PostBarCard.tsx
@@ -5,17 +5,17 @@ import example from '../../../public/images/Background/card_example.jpg';
 const PostBarCard = () => (
   <div className="flex flex-col text-white items-start relative justify-end gap-2 w-full min-h-[250px] max-h-[250px] border border-gray-700 dark:border-zinc-800 rounded-xl p-4 shadow-lg min-w-[200px] max-w-[200px]">
 
-    <h1 className="text-lg font-medium">Title</h1>
+    <h1 className="text-lg font-medium z-[3]">Title</h1>
 
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
+    <p className="z-[3]">Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
 
-    <div className="absolute inset-0 bg-black/40 z-[-1] rounded-lg" />
+    <div className="absolute inset-0 bg-black/40  z-[2] rounded-lg" />
 
     <Image
       src={example}
       alt="example"
       fill
-      className="object-cover z-[-2] rounded-lg"
+      className="object-cover z-[1] rounded-lg"
     />
 
   </div>
