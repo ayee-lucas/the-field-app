edcaf9cbe53c7b5269b7e1a0c3ee2390cb34e3c1
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-06-06 23:59:57 -0600

    PostHomeCard Improved

--- app/Home/components/PostHomeCard.tsx
+++ app/Home/components/PostHomeCard.tsx
@@ -1,37 +1,85 @@
+'use client';
+
 import Image from 'next/image';
-import React from 'react';
+import {
+  FC, useRef, useEffect, useState,
+} from 'react';
+import { useRouter } from 'next/navigation';
+import { IPost } from '@/app/models/Post';
 import imagePost from '../../../public/images/Background/card_example.jpg';
+import PostFooterActions from '../post/components/PostFooterActions';
 
-const PostHomeCard = () => (
-  <div className="w-full min-h-[500px] bg-gray-100 dark:bg-black dark:border-zinc-800 border rounded-lg p-5 my-3 grid grid-flow-row gap-2">
-    <div className="">
-      <h1 className="text-2xl font-bold">LEBRON JAMES JA MORANT</h1>
-      <p className="pt-4 text-gray-700 dark:text-gray-300">
-        The more I think about this streak, the more I believe it's the most
-        unbeatable of all records. LBJ is on an open streak of 1,151 consecutive
-        double digit Regular Season games (the streak doesn't include Playoffs
-        matches). That's 14 full 82-game seasons. To put include Playoffs
-        matches). That's 14 full 82-game seasons. To put include Playoffs
-        matches). That's 14 full 82-game seasons. To put include Playoffs
-        matches). That's 14 full 82-game seasons. To put include Playoffs
-        matches). That's 14 full 82-game seasons. To put include Playoffs
-        matches). That's 14 full 82-game seasons. To put include Playoffs
-        matches). That's 14 full 82-game seasons. To put include Playoffs
-        matches). That's 14 full 82-game seasons.
-      </p>
+interface Props {
+  post: IPost;
+}
+
+const PostHomeCard: FC<Props> = ({ post }) => {
+  const router = useRouter();
+  const postContentRef = useRef<HTMLParagraphElement>(null);
+  const [isTruncated, setIsTruncated] = useState(false);
+
+  const resizeContent = () => {
+    const element = postContentRef.current;
+    if (element && element.scrollHeight > element.clientHeight) {
+      setIsTruncated(true);
+    } else {
+      setIsTruncated(false);
+    }
+  };
+
+  useEffect(() => {
+    resizeContent();
+
+    const handleResize = () => {
+      resizeContent();
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const firstLine = post.content.text.split('\n')[0];
+  const title = firstLine.length > 100 ? `${firstLine.substring(0, 100)}...` : firstLine;
+  const body = firstLine.length > 100
+    ? post.content.text : post.content.text.substring(firstLine.length);
+
+  const handleShowMore = () => {
+    router.push('/Home');
+  };
+
+  return (
+    <div className="w-full min-h-[500px] bg-gray-100 dark:bg-black dark:border-zinc-800 border rounded-lg p-5 my-3 grid grid-flow-row gap-2">
+      <div className="relative">
+        <h1 className="text-2xl font-bold">{title}</h1>
+        <p
+          ref={postContentRef}
+          className="pt-4 text-gray-700 dark:text-gray-300 max-xl:max-h-[200px] overflow-hidden "
+        >
+          {isTruncated ? post.content.text : body}
+        </p>
+
+        {isTruncated && (
+          <button type="button" onClick={handleShowMore} className="absolute w-full text-center dark:text-zinc-500 z-40 -bottom-1 rounded-t-lg bg-black/70 backdrop-blur-sm shadow-[0px_-10px_30px_10px] shadow-black">
+            {body ? 'Show More' : 'Show Less'}
+          </button>
+        )}
+
+      </div>
+
+      <div className="relative w-full h-full min-h-[300px]">
+        <Image
+          src={imagePost}
+          alt="imagePost"
+          fill
+          className="rounded-lg  object-cover"
+        />
+      </div>
+
+      <PostFooterActions />
     </div>
-
-    <div className="relative w-full h-full min-h-[300px]">
-      <Image
-        src={imagePost}
-        alt="imagePost"
-        fill
-        className="rounded-lg  object-cover"
-      />
-    </div>
-
-    <div className="max-h-[50px]">Footer</div>
-  </div>
-);
+  );
+};
 
 export default PostHomeCard;
