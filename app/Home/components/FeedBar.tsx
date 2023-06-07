bba9efac9d774984f2767addcd77cbeaaa9892c4
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-06-06 23:59:11 -0600

    feedbar background

--- app/Home/components/FeedBar.tsx
+++ app/Home/components/FeedBar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import PostBarCard from './PostBarCard';
 
 const FeedBar = () => (
-  <div className="w-full flex flex-col gap-2 items-start justify-center py-5">
+  <div className="w-full flex flex-col gap-2 items-start justify-center py-5 bg-transparent">
     <h1 className="text-xl font-bold ">Recent</h1>
     <div className="w-full flex items-center justify-between gap-2 overflow-x-auto">
       {Array.from({ length: 5 }).map((_, index) => (
