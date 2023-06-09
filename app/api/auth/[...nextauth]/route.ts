e1fc5915707751b173aba21fce238b2ad6ad34d5
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-06-08 18:25:15 -0600

    hardcoded url

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -20,7 +20,7 @@ export const authOptions: NextAuthOptions = {
         password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials) {
-        const res = await fetch(`${url}/api/account/login/`, {
+        const res = await fetch('https://the-field-app.vercel.app/api/account/login', {
           method: 'POST',
           body: JSON.stringify(credentials),
           headers: { 'Content-Type': 'application/json' },
