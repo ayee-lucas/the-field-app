6d10975bc70b9714fdcfbe78cba4843865a8806f
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-06-11 12:05:23 -0600

    Follow and Message profile buttons dark mode added

--- app/Home/profile/[user]/components/Followbtn.tsx
+++ app/Home/profile/[user]/components/Followbtn.tsx
@@ -1,12 +1,16 @@
 'use client';
 
 import React from 'react';
-import { GrAddCircle } from 'react-icons/gr';
+import { IoMdAddCircleOutline } from 'react-icons/io';
 
 function Followbtn() {
   return (
-    <button type="button" className="py-1 px-5 rounded-xl cursor-pointer  bg-gray-200 flex items-center gap-2 hover:bg-gray-300 max-w-[130px] my-4">
-      <GrAddCircle />
+    <button
+      type="button"
+      className="py-1 px-5 rounded-xl cursor-pointer  bg-gray-200 flex items-center gap-2 hover:bg-gray-300 max-w-[130px] my-4
+      dark:bg-zinc-900 dark:text-gray-300 border dark:border-gray-300"
+    >
+      <IoMdAddCircleOutline />
       Follow
     </button>
   );
--- app/Home/profile/[user]/components/Messagebtn.tsx
+++ app/Home/profile/[user]/components/Messagebtn.tsx
@@ -3,7 +3,11 @@ import { TbMessageCircle2Filled } from 'react-icons/tb';
 
 function Messagebtn() {
   return (
-    <button type="button" className="py-1 px-5 rounded-xl cursor-pointer bg-gray-200 flex items-center gap-2 hover:bg-gray-300 max-w-[140px] my-4">
+    <button
+      type="button"
+      className="py-1 px-5 rounded-xl cursor-pointer bg-gray-200 flex items-center gap-2 hover:bg-gray-300 max-w-[140px] my-4
+      dark:bg-zinc-900 dark:text-gray-300 border dark:border-gray-300"
+    >
       <TbMessageCircle2Filled />
       Message
     </button>
