7ead969460e5179eb4ec5d721ee4cd37230c01a9
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-06-11 22:34:06 -0600

    textarea on post improvements

--- app/Home/post/components/TextArea.tsx
+++ app/Home/post/components/TextArea.tsx
@@ -18,7 +18,7 @@ const TextArea = ({ setText }
         rows={10}
         placeholder="What's on your mind?"
         className="w-full p-2 my-2 text-sm rounded-md border border-gray-300 focus:outline-none focus:border-gray-400
-                      dark:bg-transparent dark:text-white dark:border-zinc-700 first-line:text-3xl resize-none"
+                      dark:bg-transparent dark:text-white dark:border-zinc-700 first-line:text-xl first-line:font-bold resize-none"
 
       />
 
