03029b32a656f509f3fb5c7609c1fdd4d5ce100a
ayee-lucas <nouminosxz@gmail.com>
2023-08-07 02:40:33 -0700

    Added context error to config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,1 +1,3 @@
 export const SCROLLING_PAGINATION_NUMBER = 3;
+export const NEWPOST_CONTEXT_ERROR =
+  'Unable to retrieve data from NewPostContext. Please check if the context is properly set up and provided.';
