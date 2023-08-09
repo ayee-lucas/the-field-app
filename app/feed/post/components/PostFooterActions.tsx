2197a445644127693add46628103e1ef7e60bf21
ayee-lucas <nouminosxz@gmail.com>
2023-08-09 02:47:50 -0700

    Unset all the functions

--- app/feed/post/components/PostFooterActions.tsx
+++ app/feed/post/components/PostFooterActions.tsx
@@ -1,87 +1,74 @@
 'use client';
 
-import {
-  FC, useTransition, useState,
-} from 'react';
+import { FC, useTransition, useState } from 'react';
 import { AiOutlineHeart, AiOutlineStar, AiFillHeart } from 'react-icons/ai';
 import { BiRepost } from 'react-icons/bi';
 import { FaRegComment } from 'react-icons/fa';
 import { RiSendPlaneLine } from 'react-icons/ri';
 import { useRouter } from 'next/navigation';
-import { PostType } from '@/app/types/postType';
-import { dislikePost, likePost } from '../actions';
+import { ExtendedPost } from '@/app/types/postType';
+import { REQUIREMENT_NOTFOUND } from '@/app/config';
 
-interface Props {
-  // eslint-disable-next-line react/require-default-props
-  onClick: () => void;
-  // eslint-disable-next-line react/require-default-props
-  Post: PostType;
-  // eslint-disable-next-line react/require-default-props
+type Props = {
+  post: ExtendedPost;
   sessionId: string;
-}
+};
 
-const PostFooterActions:FC<Props> = ({ onClick, Post, sessionId }) => {
+const PostFooterActions: FC<Props> = ({ post, sessionId }) => {
+  if (!post) {
+    throw new Error(`${REQUIREMENT_NOTFOUND} [POST]`);
+  }
+
+  if (!post.Like) {
+    throw new Error(`${REQUIREMENT_NOTFOUND} [POST LIKES]`);
+  }
+  const LikesArr = post.Like;
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [isPending, startTransition] = useTransition();
 
-  const [likes, setLikes] = useState<number>(Post.likes.length);
+  const [likes, setLikes] = useState<number>(LikesArr.length);
 
-  const [isLiked, setIsLiked] = useState<boolean>(
-    Boolean(Post.likes.find((like) => like._id === sessionId)),
-  );
+  const [isLiked, setIsLiked] = useState<boolean>(false);
 
   const router = useRouter();
 
   const handleLiked = () => {
-    if (!sessionId) {
-      router.push('/auth/signin');
-    } else {
-      setLikes(likes + 1);
-      setIsLiked(true);
-      startTransition(() => {
-        likePost(Post?._id, sessionId);
-      });
-    }
+    console.log('Like');
   };
 
   const handleDislike = () => {
-    if (!sessionId) {
-      router.push('/auth/sigin');
-    } else {
-      setLikes(likes - 1);
-      setIsLiked(false);
-      startTransition(() => {
-        dislikePost(Post?._id, sessionId);
-      });
-    }
+    console.log('Disklike');
   };
 
-  //  const isLiked = Post?.likes.find((like) => like._id === sessionId);
-
   return (
-
     <div className="flex justify-between items-center gap-3 py-1 px-2 w-full border-t border-t-gray-300 dark:border-t-zinc-600 text-xl text-gray-600 dark:text-gray-300">
       <div className="flex items-center gap-3 w-full h-full">
         <div className="flex items-center gap-1">
-          { isLiked
-            ? <AiFillHeart className="cursor-pointer text-red-600" onClick={handleDislike} />
-            : <AiOutlineHeart className="cursor-pointer " onClick={handleLiked} />}
+          {isLiked ? (
+            <AiFillHeart
+              className="cursor-pointer text-red-600"
+              onClick={handleDislike}
+            />
+          ) : (
+            <AiOutlineHeart className="cursor-pointer " onClick={handleLiked} />
+          )}
           <span className="text-sm">{likes === 0 ? '0' : likes}</span>
         </div>
 
         <div className="flex items-center gap-1">
           <BiRepost className="cursor-pointer" />
-          <span className="text-sm">{Post.repost.length}</span>
+          <span className="text-sm">0</span>
         </div>
 
         <div className="flex items-center gap-1">
           <AiOutlineStar className="cursor-pointer" />
-          <span className="text-sm">{Post.starred.length}</span>
+          <span className="text-sm">0</span>
         </div>
 
         <div className="flex items-center gap-1">
-          <FaRegComment onClick={onClick} className="cursor-pointer" size={16} />
-          <span className="text-sm">{Post.comments.length}</span>
+          <FaRegComment className="cursor-pointer" size={16} />
+          <span className="text-sm">0</span>
         </div>
       </div>
       <div className="flex items-center gap-3 mt-3 max-sm:mt-0 cursor-pointer text-sm text-white dark:text-fieldGreen bg-fieldGreen dark:bg-black border dark:border-fieldGreen px-5 py-1 rounded-full">
