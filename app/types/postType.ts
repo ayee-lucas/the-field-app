8f6abf37062cfb490982bd7024c8ad9c910d0fc8
ayee-lucas <nouminosxz@gmail.com>
2023-08-09 02:48:26 -0700

    deleted old type and added the new type for posts

--- app/types/postType.ts
+++ app/types/postType.ts
@@ -1,24 +1,6 @@
-export type PostType = {
-  _id: string;
-  author: {
-    _id: string;
-    username: string;
-    name: string;
-    picture: {
-      pictureKey: string;
-      pictureURL: string
-    }
-  }
-  content: {
-    text: string;
-    media: string[];
-  }
-  repost: string[];
-  starred: string[];
-  createdAt: Date;
-  comments: string[];
-  likes: [{
-    _id: string;
-    username: string;
-  }]
+import { Post, User, Like } from '@prisma/client';
+
+export type ExtendedPost = Post & {
+  Author?: User;
+  Like?: Like[];
 };
