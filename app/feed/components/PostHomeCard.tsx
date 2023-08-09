3a6be017ede80168aa5d85eab11a4c988b9ca3f8
ayee-lucas <nouminosxz@gmail.com>
2023-08-09 02:47:21 -0700

    redone to work with prisma

--- app/feed/components/PostHomeCard.tsx
+++ app/feed/components/PostHomeCard.tsx
@@ -6,23 +6,38 @@ import { useRouter } from 'next/navigation';
 import PostFormatted from '@/app/tools/postFormatter';
 import Link from 'next/link';
 import { formatDate } from '@/app/tools/datesFormatter';
-import { PostType } from '@/app/types/postType';
 import imagePost from '@/public/images/Background/card_example.jpg';
 import CustomAvatar from '@/components/ui/custom-avatar';
+import { ExtendedPost } from '@/app/types/postType';
+import { getProfile } from '@/app/server-actions/profile/actions';
+import { REQUIREMENT_NOTFOUND } from '@/app/config';
+import { Profile } from '@prisma/client';
 import PostFooterActions from '../post/components/PostFooterActions';
+import PostLoading from './PostLoading';
 
-interface Props {
-  post: PostType;
+type Props = {
+  post: ExtendedPost;
   sessionId: string;
-}
+};
 
 const PostHomeCard: FC<Props> = ({ post, sessionId }) => {
   const router = useRouter();
+
   const postContentRef = useRef<HTMLParagraphElement>(null);
 
   const [isTruncated, setIsTruncated] = useState(false);
 
-  console.log(post.author.picture.pictureURL);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const [userProfile, setUserProfile] = useState<Profile | undefined>(
+    undefined
+  );
+
+  if (!post.Author) {
+    throw new Error(`${REQUIREMENT_NOTFOUND} [POST AUTHOR]`);
+  }
+
+  const profileId = post.Author.profile_id;
 
   const resizeContent = () => {
     const element = postContentRef.current;
@@ -46,28 +61,48 @@ const PostHomeCard: FC<Props> = ({ post, sessionId }) => {
     };
   }, []);
 
+  useEffect(() => {
+    const getData = async () => {
+      const profile = await getProfile(profileId);
+
+      if ('error' in profile) {
+        throw new Error(`${REQUIREMENT_NOTFOUND} [PROFILE]`);
+      }
+
+      setUserProfile(profile.data);
+
+      return setLoading(false);
+    };
+
+    getData();
+  }, [profileId]);
+
   const { title, body } = PostFormatted(post.content.text);
 
-  const { formatedDate, formatedTime } = formatDate(post.createdAt);
+  const { formatedDate, formatedTime } = formatDate(post.created_at);
 
   const redirect = () => {
-    router.push(`/Home/post/${post.author.username}/${post._id}/`);
+    router.push(`/Home/post/${post.author_id}/${post.id}/`);
   };
+
+  if (loading) {
+    return <PostLoading />;
+  }
 
   return (
     <div
-      className="w-full min-h-fit bg-gray-100 dark:bg-black dark:border-zinc-800 border rounded-lg p-5 my-3 grid grid-flow-row gap-2"
+      className="w-full min-h-fit bg-gray-100 dark:bg-black dark:border-zinc-800 border rounded-lg p-5 my-3 grid grid-flow-row gap-2 overflow-hidden"
       aria-hidden
     >
       <Link
         className="w-full flex items-center gap-2"
-        href={`/Home/profile/${post.author.username}`}
+        href={`/Home/profile/${post.author_id}`}
       >
-        <CustomAvatar imgUrl={post.author.picture.pictureURL} />
+        <CustomAvatar imgUrl={post.Author.picture.pictureURL} />
 
-        {post.author.name}
+        {userProfile?.name}
         <span className="text-sm text-gray-700 dark:text-zinc-500">
-          @{post.author.username}
+          @{post.Author.username}
         </span>
       </Link>
       <div className="relative">
@@ -90,7 +125,7 @@ const PostHomeCard: FC<Props> = ({ post, sessionId }) => {
         )}
       </div>
 
-      {post.content.media.length > 0 && (
+      {post.content.media.MediaURL.length > 0 && (
         <div className="relative w-full h-full min-h-[300px]">
           <Image
             src={imagePost}
@@ -101,12 +136,12 @@ const PostHomeCard: FC<Props> = ({ post, sessionId }) => {
         </div>
       )}
 
+      <PostFooterActions post={post} sessionId={sessionId} />
+
       <div className="flex items-center text-sm text-gray-700 dark:text-zinc-500 gap-3 w-full">
         <span>{formatedTime}</span>
         <span>{formatedDate}</span>
       </div>
-
-      <PostFooterActions onClick={redirect} Post={post} sessionId={sessionId} />
     </div>
   );
 };
