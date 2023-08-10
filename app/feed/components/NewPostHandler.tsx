f47aa8c1177d365e89df00c6e0f02d5e9bc3c5ac
ayee-lucas <nouminosxz@gmail.com>
2023-08-09 23:10:05 -0700

    added progress state por progress bar

--- app/feed/components/NewPostHandler.tsx
+++ app/feed/components/NewPostHandler.tsx
@@ -1,30 +1,35 @@
 'use client';
 
-import { useState, createContext, SetStateAction, useMemo } from 'react';
+import React, { useState, createContext, SetStateAction, useMemo } from 'react';
 import PostEditor from './PostEditor';
 import NewPostButton from './NewPostButton';
 
 type Context = {
   loading: boolean;
   toggleEditor: boolean;
+  progress: number;
   setLoading: React.Dispatch<SetStateAction<boolean>>;
   setToggleEditor: React.Dispatch<SetStateAction<boolean>>;
+  setProgress: React.Dispatch<SetStateAction<number>>;
 };
 
 export const NewPostContext = createContext<Context | undefined>(undefined);
 
-export default function NewPostHandler() {
+export default function NewPostHandlera() {
   const [loading, setLoading] = useState<boolean>(false);
   const [toggleEditor, setToggleEditor] = useState<boolean>(false);
+  const [progress, setProgress] = useState<number>(0);
 
   const values = useMemo(
     () => ({
       loading,
       setLoading,
+      progress,
+      setProgress,
       toggleEditor,
       setToggleEditor,
     }),
-    [loading, setLoading, toggleEditor, setToggleEditor]
+    [loading, setLoading, toggleEditor, setToggleEditor, progress, setProgress]
   );
 
   return (
