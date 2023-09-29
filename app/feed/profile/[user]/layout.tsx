349dcb59692af5b95afe328e803d2e25c7d19e98
ayee-lucas <nouminosxz@gmail.com>
2023-09-28 23:42:41 -0600

    migration fixed

--- app/feed/profile/[user]/layout.tsx
+++ app/feed/profile/[user]/layout.tsx
@@ -2,32 +2,46 @@
 import { RxDotFilled } from 'react-icons/rx';
 import Image from 'next/image';
 import { getGoSession } from '@/app/tools/getGoServerSession';
-import { Session } from '@/app/types/sessionType';
 import defaultImage from '@/public/images/default_user.png';
-import { UserType } from '@/app/types/userType';
+import { getProfile } from '@/app/server-actions/profile/actions';
+import NewPostHandler from '@/app/feed/components/NewPostHandler';
+import { goGetUserById } from '@/app/server-actions/signin/actions';
+import { redirect } from 'next/navigation';
+import { ROUTES } from '@/app/config';
 import Followbtn from './components/Followbtn';
-import { getProfile } from './actions/actions';
 import Messagebtn from './components/Messagebtn';
 import NavProfile from './components/NavProfile';
 import Editbtn from './components/Editbtn';
 import Settigns from './components/Settigns';
-import ClientNewPost from '../../components/ClientNewPost';
 
 export default async function ProfileLayout({
-  params,
   children,
 }: {
-  params: { user: string };
   children: React.ReactNode;
 }) {
   const goSession = await getGoSession();
 
-  const userget: string = await getProfile(params.user);
+  if (!goSession?.user) {
+    redirect(ROUTES.signin);
+  }
 
-  const user: UserType = JSON.parse(userget);
+  const user = await goGetUserById(goSession?.user?.id);
 
-  if (!user) {
-    return <div>Loading...</div>;
+  if ('error' in user) {
+    redirect(ROUTES.signin);
+  }
+
+  if (!user.data) {
+    redirect(ROUTES.signin);
+  }
+
+  if (!user.data.picture) {
+    redirect(ROUTES.signin);
+  }
+
+  const profile = await getProfile(user.data.profile_id);
+  if ('error' in profile) {
+    redirect(ROUTES.signin);
   }
 
   return (
@@ -41,7 +55,7 @@ export default async function ProfileLayout({
             className="absolute rounded-lg w-full h-full object-cover"
           />
 
-          {goSession?.user?.username !== user.username ? null : (
+          {goSession?.user?.username !== user.data.username ? null : (
             <div className="opacity-0 text-sm hover:opacity-100 absolute bottom-0 left-0 w-full h-1/4 transition-all z-[1] cursor-pointer bg-black/70 text-white flex justify-center items-center">
               Edit cover
             </div>
@@ -50,16 +64,12 @@ export default async function ProfileLayout({
           <div className="absolute mt-28 ml-7 max-sm:ml-3 max-sm:mt-32">
             <div className="relative z-[2] rounded-full h-full w-[180px] min-h-[180px] max-sm:w-[120px] max-sm:min-h-[120px] dark:bg-zinc-900 bg-gray-100">
               <Image
-                src={
-                  user.picture.pictureURL
-                    ? user.picture.pictureURL
-                    : defaultImage
-                }
+                src={user.data ? user.data.picture.pictureURL : defaultImage}
                 alt="user image"
                 fill
                 className="absolute w-full h-full z-[2] object-cover rounded-full border-[3px] border-gray-700"
               />
-              {goSession?.user?.username !== user.username ? null : (
+              {goSession?.user?.username !== user.data.username ? null : (
                 <div className="opacity-0 text-sm hover:opacity-100 absolute bottom-0 left-0 w-full h-1/2 transition-all z-[3] rounded-b-full cursor-pointer bg-black/70 text-white flex justify-center items-center">
                   Edit picture
                 </div>
@@ -71,10 +81,10 @@ export default async function ProfileLayout({
       <div className="w-full h-full flex flex-col px-3 pt-24 max-sm:pt-14 text-black">
         <div className="flex w-full h-full justify-between items-center gap-5">
           <h1 className="text-6xl max-sm:text-3xl dark:text-white">
-            {user.name}
+            {user.data.username}
           </h1>
-          {goSession?.user?.username !== user.username ? (
-            user.online ? (
+          {goSession?.user?.username !== user.data.username ? (
+            profile.data.online ? (
               <div className="text-lg max-sm:text-sm text-gray-500 flex items-center gap-1">
                 <RxDotFilled className="text-green-500" />
                 ONLINE
@@ -94,17 +104,17 @@ export default async function ProfileLayout({
         </div>
         <div className="flex w-full h-full justify-start items-center gap-5 select-none max-sm:text-sm">
           <h1 className="px-2 text-xl max-sm:text-sm text-gray-500 max-sm:px-0">
-            @{user.username}
+            @{user.data.username}
           </h1>
           <h1 className=" text-gray-800 max-sm:text-gray-500 cursor-pointer">
-            {user.followers.length} followers
+            100 followers
           </h1>
 
           <h1 className=" text-gray-800 max-sm:text-gray-500 cursor-pointer">
-            {user.followers.length} following
+            100 following
           </h1>
         </div>
-        {goSession?.user?.username !== user.username ? (
+        {goSession?.user?.username !== user.data.username ? (
           <div className="flex w-full h-full justify-start items-center gap-5">
             <Followbtn />
             <Messagebtn />{' '}
@@ -120,12 +130,12 @@ export default async function ProfileLayout({
           <h1 className="text-lg text-black font-semibold dark:text-white">
             BIO
           </h1>
-          <p className="text-gray-600 dark:text-zinc-400">{user.bio}</p>
+          <p className="text-gray-600 dark:text-zinc-400">{profile.data.bio}</p>
         </div>
 
         <NavProfile />
-        {goSession?.user?.username !== user.username ? null : (
-          <ClientNewPost session={goSession as Session} />
+        {goSession?.user?.username !== user.data.username ? null : (
+          <NewPostHandler />
         )}
         <div className="px-2">{children}</div>
       </div>
